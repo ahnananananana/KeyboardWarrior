77080d8856a659480ea2e81676f40fc0b25c162c
LeeGilsu <lgs731@naver.com>
2020-07-01 15:48:23 +0900

    일단 푸쉬 파티클 충돌 진행중.

--- Assets/DaeHan/Scripts/CharCtrl.cs
+++ Assets/DaeHan/Scripts/CharCtrl.cs
@@ -2,27 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharCtrl : Character
+public class CharCtrl : MonoBehaviour
 {
-    public enum CTRLSTATE // 캐릭터 상태
+    public enum STATE // 캐릭터 상태
     {
-        CREATE, IDLE, WALK, ROLL, ATTACK, DEAD
+        CREATE,IDLE,WALK,ROLL,ATTACK,DEAD
     }
-    public CTRLSTATE ctrlstate = CTRLSTATE.CREATE;
+    public STATE state = STATE.CREATE;
 
     public enum WEAPONTYPE // 사용중인 무기 종류
     {
-        BOW, GUN, SWORD
+        BOW,GUN,SWORD
     }
     public WEAPONTYPE weapontype = WEAPONTYPE.BOW;
 
-    public enum LAYERSTATE
-    {
-        PLAYER, IMMORTAL
-    }
-    LAYERSTATE layerstate = LAYERSTATE.PLAYER;
-
-    protected struct MoveData // 움직임 관여 타겟과 자신의 거리, 트랜스폼정보
+    protected struct MoveData
     {
         public Vector3 TargetPosition;
         public Vector3 CurrentPosition;
@@ -30,78 +24,81 @@ public class CharCtrl : Character
         public Vector3 MoveDir;
         public float MoveDist;
         public float RotY;
-
     }
     protected MoveData m_MoveData;
 
-    public GameObject Weapon_Obj; // 현재 장착중인 무기
-    public GameObject Bow_Obj; // 삭제할 임시 변수
-    public GameObject Gun_Obj; // 삭제할 임시 변수
-    public GameObject Sword_Obj; // 삭제할 임시 변수
-    public GameObject Arrow_Obj; // 화살 오브젝트
-    public GameObject Bullet_Obj; // 총알 오브젝트
-    public GameObject SwordAtkBox; // 근접공격시 나올 충돌박스
+    public GameObject Weapon_Obj;
+    public GameObject Bow_Obj;
+    public GameObject Gun_Obj;
+    public GameObject Sword_Obj;
+    public GameObject Arrow_Obj;
+    public GameObject Bullet_Obj;
+    public GameObject SwordAtkBox;
 
-    public Transform UsingWeaponTR; // 사용중인 무기의 트랜스폼
-    public Transform BowTR; // 활이 장착될 위치
-    public Transform GunTR; // 총이 장착될 위치
-    public Transform SwordTR; // 검이 장착될 위치
-    public Transform ArrowMuzzleTR; // 화살이 나갈 위치
-    public Transform BulletMuzzleTR; // 총알이 나갈 위치
-    public Transform SwordAtkBoxTR; // 근접공격 충돌박스가 나올 위치
+    public Transform UsingWeaponTR;
+    public Transform BowTR;
+    public Transform GunTR;
+    public Transform SwordTR;
+    public Transform ArrowMuzzleTR;
+    public Transform BulletMuzzleTR;
+    public Transform SwordAtkBoxTR;
 
-    Vector3 MousePos = Vector3.zero;
+    public GameObject TargettingMonster;
 
-    public GameObject TargettingMonster; // 현재 타겟팅중인 몬스터
+    public bool isAttacking = false;
+    public bool isMoving = false;
 
-    public bool isAttacking = false; // 공격중 bool값
-    public bool isMoving = false; // 이동중 bool값
-    public bool bLongDistAtk = false; // 근, 원거리 공격 bool값
+    public bool bLongDistAtk = false;
 
-    public float m_RotSpeed = 400f; // 이동중의 캐릭터 회전 속도
-    public float m_AttackRotSpeed = 1200f; // 공격시 타겟으로 회전하는 속도
-    public float m_RollSpeed = 10.0f; // 구르기 속도
-    public float MoveSpeed = 5.0f; // 삭제할 변수
+    public float m_RotSpeed = 400f;
+    public float m_AttackRotSpeed = 1200f;
+    public float m_RollSpeed = 10.0f;
 
-    public LayerMask m_GroundLayer; // 땅 오브젝트를 레이어로 구분하기위함
-    public LayerMask m_MonsterLayer; // 몬스터 오브젝트를 레이어로 구분하기 위함
+    //----------삭제할 변수----------//
+    public float m_MoveSpeed = 10.0f;
+    //-------------------------------//
 
-    public Camera m_MainCamera; // 메인 카메라가 들어갈 변수
+    public LayerMask m_GroundLayer;
+    public LayerMask m_MonsterLayer;
 
-    public Animator UsingAni; // 사용중인 애니메이션
-    public RuntimeAnimatorController BowAni; // 활을 들었을 때 애니메이션
-    public RuntimeAnimatorController GunAni; // 총을 들었을 때 애니메이션
-    public RuntimeAnimatorController SwordAni; // 칼을 들었을 때 애니메이션
+    public Camera m_MainCamera;
+    
+    public Animator UsingAni;
+    public RuntimeAnimatorController BowAni;
+    public RuntimeAnimatorController GunAni;
+    public RuntimeAnimatorController SwordAni;
+    public Projectile proj;
 
-    public HandAtk handatk;
-    public Projectile proj; // 투사체 스크립트
-    public Player player;
-    public Character character; // 캐릭터 스크립트
-
+    public Character character;
     private void Awake()
     {
         UsingWeaponTR = BowTR;
     }
-
-    new void Start()
+    private void Start()
     {
         UsingAni = GetComponent<Animator>();
         ChangeWeapon(WEAPONTYPE.BOW, Bow_Obj);
     }
 
-    private void Update()
+    private void Update()   
     {
-        CtrlStateProcess();
+        StateProcess();
     }
 
-    protected virtual void CtrlStateProcess()
+    private void OnParticleCollision(GameObject other)
     {
-        switch (ctrlstate)
+        Debug.Log("파이클");
+    }
+
+
+    protected virtual void StateProcess()
+    {
+        switch (state)
         {
-            case CTRLSTATE.CREATE:
-                ChangeCtrlState(CTRLSTATE.IDLE);
+            case STATE.CREATE:
+                ChangeState(STATE.IDLE);
                 break;
-            case CTRLSTATE.IDLE:
+            case STATE.IDLE:
                 if (Input.GetKey(KeyCode.LeftControl) && Input.GetMouseButton(0))
                 {
                     Picking(true);
@@ -119,7 +116,7 @@ public class CharCtrl : Character
 
                 if (Input.GetKeyDown(KeyCode.D))
                 {
-                    ChangeCtrlState(CTRLSTATE.ROLL);
+                    ChangeState(STATE.ROLL);
                 }
 
                 //------------------지울거-------------------//
@@ -135,58 +132,60 @@ public class CharCtrl : Character
                 {
                     ChangeWeapon(WEAPONTYPE.SWORD, Sword_Obj);
                 }
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    ChangeState(STATE.DEAD);
+                }
                 //-------------------------------------------//
                 break;
-            case CTRLSTATE.WALK:
+            case STATE.WALK:
                 if (Input.GetMouseButtonUp(0))
                 {
                     isMoving = false;
                 }
                 Rotating(m_RotSpeed);
-                Moving();
+                Moving(true);
                 break;
-            case CTRLSTATE.ROLL:
+            case STATE.ROLL:
                 if (Input.GetMouseButtonUp(0))
                 {
                     isMoving = false;
                 }
                 Roll();
                 break;
-            case CTRLSTATE.ATTACK:
+            case STATE.ATTACK:
                 if (Input.GetMouseButtonUp(0))
                 {
                     isAttacking = false;
                 }
                 Rotating(m_AttackRotSpeed);
                 break;
-            case CTRLSTATE.DEAD:
+            case STATE.DEAD:
                 break;
         }
     }
 
-    protected virtual void ChangeCtrlState(CTRLSTATE s)
+    protected virtual void ChangeState(STATE s)
     {
-        ctrlstate = s;
+        if (state == s) return;
+        state = s;
 
         switch (s)
         {
-            case CTRLSTATE.CREATE:
+            case STATE.CREATE:
                 break;
-            case CTRLSTATE.IDLE:
+            case STATE.IDLE:
                 break;
-            case CTRLSTATE.WALK:
+            case STATE.WALK:
                 ReadyMove();
                 break;
-            case CTRLSTATE.ROLL:
-                MousePicking();
-                this.transform.LookAt(MousePos, this.transform.up);
+            case STATE.ROLL:
                 UsingAni.SetTrigger("Roll");
-                ChangeLayer(LAYERSTATE.IMMORTAL);
                 break;
-            case CTRLSTATE.ATTACK:
+            case STATE.ATTACK:
                 Attack();
                 break;
-            case CTRLSTATE.DEAD:
+            case STATE.DEAD:
                 Dead();
                 break;
         }
@@ -220,37 +219,23 @@ public class CharCtrl : Character
         Weapon_Obj.transform.rotation = UsingWeaponTR.rotation;
     }
 
-    void ChangeLayer(LAYERSTATE s)
+    protected void Roll()
     {
-        layerstate = s;
-
-        switch (s)
-        {
-            case LAYERSTATE.PLAYER:
-                this.gameObject.layer = LayerMask.NameToLayer("Player");
-                break;
-            case LAYERSTATE.IMMORTAL:
-                this.gameObject.layer = LayerMask.NameToLayer("Immortal");
-                break;
-        }
-    }
-
-    protected void Roll() // 구르기
-    {
-        Vector3 pos = transform.position;
+        Vector3 pos = transform.localPosition;
         float delta = m_RollSpeed * Time.smoothDeltaTime;
         Vector3 target = pos + this.transform.forward * delta;
-        transform.position = target;
+
+        transform.localPosition = target;
     }
 
-    protected void Attack() // 공격
+    protected void Attack()
     {
         ReadyMove();
-        UsingAni.SetFloat("AttackSpeed", player.m_AttackSpeed.m_CurrentValue);
+        //Ani.Play("Idle");
         UsingAni.SetTrigger("Attack");
     }
 
-    protected void BowFire() // 활 공격 (애니메이션 이벤트로 실행)
+    protected void BowFire()
     {
         GameObject obj = Instantiate(Arrow_Obj) as GameObject;
         proj = obj.GetComponent<Projectile>();
@@ -260,88 +245,57 @@ public class CharCtrl : Character
         proj.OnFire(ArrowMuzzleTR.forward);
     }
 
-    protected void GunFire() // 총 공격 (애니메이션 이벤트로 실행)
+    protected void GunFire()
     {
         GameObject obj = Instantiate(Bullet_Obj) as GameObject;
         proj = obj.GetComponent<Projectile>();
         obj.transform.position = BulletMuzzleTR.position;
         obj.transform.rotation = this.transform.rotation;
-        proj.character = GetComponent<Player>();
         proj.OnFire(BulletMuzzleTR.forward);
     }
 
-    protected void SwordAttack() // 검 공격 (애니메이션 이벤트로 실행)
+    protected void SwordAttack()
     {
-        GameObject obj = Instantiate(SwordAtkBox) as GameObject;
-        handatk = obj.GetComponent<HandAtk>();
+        GameObject obj = Instantiate(SwordAtkBox);
         obj.transform.position = SwordAtkBoxTR.transform.position;
         obj.transform.rotation = SwordAtkBoxTR.transform.rotation;
-        handatk.character = GetComponent<Player>();
     }
 
-    protected void Dead() // 죽음
+    protected void Dead()
     {
         StopAllCoroutines();
         UsingAni.Play("Death");
-    }
+    }  
 
-    protected void MousePicking()
-    {
-        if (m_MainCamera == null) m_MainCamera = Camera.main;
-
-        Ray ray = m_MainCamera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, 1000.0f, m_GroundLayer))
-        {
-            MousePos.x = hit.point.x;
-            MousePos.z = hit.point.z;
-        }
-    }
-
-    protected void Picking(bool LeftControl) // 캐릭터 이동&공격시 클릭을 하면 호출할 함수
+    protected void Picking(bool LeftControl)
     {
         if (m_MainCamera == null) m_MainCamera = Camera.main;
         Ray ray = m_MainCamera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        if (LeftControl == false && isAttacking == false && isMoving == false && Physics.Raycast(ray, out hit, 1000.0f, m_MonsterLayer)) // 타겟팅 공격 시작시 1회 호출되어 몬스터의 정보를 담기 위함
+        if (LeftControl == false && isAttacking == false && isMoving == false && Physics.Raycast(ray, out hit, 1000.0f, m_MonsterLayer))
         {
             isAttacking = true;
+
+            Vector3 pos = transform.position;
+            pos.x = hit.transform.gameObject.transform.position.x;
+            pos.z = hit.transform.gameObject.transform.position.z;
 
             TargettingMonster = hit.transform.gameObject;
             m_MoveData.TargetPosition = TargettingMonster.transform.position;
 
-            ReadyMove();
+            if (UsingAni.GetBool("Walk") == true)
+                UsingAni.SetBool("Walk", false);
 
-            if (bLongDistAtk == false && m_MoveData.MoveDist > 4f)
-            {
-                ChangeCtrlState(CTRLSTATE.WALK);
-            }
-            else
-            {
-                if (UsingAni.GetBool("Walk") == true)
-                    UsingAni.SetBool("Walk", false);
-                ChangeCtrlState(CTRLSTATE.ATTACK);
-            }
+            ChangeState(STATE.ATTACK);
         }
-        else if (LeftControl == false && isAttacking == true && isMoving == false && Physics.Raycast(ray, out hit, 1000.0f)) // 타겟팅 공격시 마우스를 쭉 누르고 있을 떄
+        else if (LeftControl == false && isAttacking == true && isMoving == false && Physics.Raycast(ray, out hit, 1000.0f))
         {
             m_MoveData.TargetPosition = TargettingMonster.transform.position;
-            ReadyMove();
 
-            if (bLongDistAtk == false && m_MoveData.MoveDist > 4f /*사정거리*/)
-            {
-                ChangeCtrlState(CTRLSTATE.WALK);
-            }
-            else
-            {
-                if (UsingAni.GetBool("Walk") == true)
-                    UsingAni.SetBool("Walk", false);
-                ChangeCtrlState(CTRLSTATE.ATTACK);
-            }
+            ChangeState(STATE.ATTACK);
         }
-        else if (Physics.Raycast(ray, out hit, 1000.0f, m_GroundLayer)) // 논타겟 공격 or 이동
+        else if (Physics.Raycast(ray, out hit, 1000.0f, m_GroundLayer))
         {
             Vector3 pos = transform.position;
             pos.x = hit.point.x;
@@ -352,22 +306,23 @@ public class CharCtrl : Character
             {
                 case false:
                     isMoving = true;
-                    ChangeCtrlState(CTRLSTATE.WALK);
+                    ChangeState(STATE.WALK);
                     break;
                 case true:
                     isAttacking = true;
-                    ChangeCtrlState(CTRLSTATE.ATTACK);
+                    ChangeState(STATE.ATTACK);
                     break;
             }
         }
     }
 
-    protected void ReadyMove() // 이동전 목표위치 갱신함수
+    protected void ReadyMove()
     {
         m_MoveData.MoveDir = m_MoveData.TargetPosition - transform.position;
         m_MoveData.MoveDir.Normalize();
         m_MoveData.MoveDist = Vector3.Distance(transform.position, m_MoveData.TargetPosition);
         m_MoveData.CurrentPosition = transform.position;
+
         m_MoveData.RotY = Mathf.Acos(Vector3.Dot(transform.forward, m_MoveData.MoveDir)) * (180.0f / Mathf.PI);
 
         if (Vector3.Dot(transform.right, m_MoveData.MoveDir) < 0f)
@@ -378,36 +333,37 @@ public class CharCtrl : Character
         m_MoveData.CurrentRot = transform.localRotation.eulerAngles;
     }
 
-    protected void Moving() // 이동함수
+    protected void Moving(bool bPlayer)
     {
         if (UsingAni.GetBool("Walk") == false)
         {
-            UsingAni.SetFloat("WalkSpeed", player.m_MoveSpeed.m_CurrentValue);
             UsingAni.SetBool("Walk", true);
         }
 
-        if (Input.GetMouseButton(0)) // 이동중일 때 다른 입력을 받기 위함
+        if (bPlayer) // 이동중일 때 다른 입력을 받기 위함
         {
-            ChangeCtrlState(CTRLSTATE.WALK);
+            if (Input.GetMouseButton(0))
+            {
+                ChangeState(STATE.IDLE);
 
-            if (Input.GetKey(KeyCode.LeftControl))
+                if (Input.GetKey(KeyCode.LeftControl))
+                {
+                    UsingAni.SetBool("Walk", false);
+                    Picking(true);
+                }
+                else
+                    Picking(false);
+            }
+
+            if (Input.GetKey(KeyCode.D))
             {
                 UsingAni.SetBool("Walk", false);
-                Picking(true);
+                UsingAni.Play("Idle"); // 구르기 시전 딜레이때문에 넣음
+                ChangeState(STATE.ROLL);
             }
-            else
-                Picking(false);
         }
 
-        if (Input.GetKey(KeyCode.D)) // 이동중일 때 다른 입력을 받기 위함
-        {
-            UsingAni.SetBool("Walk", false);
-            UsingAni.Play("Idle"); // 구르기 시전 딜레이때문에 넣음
-            ChangeCtrlState(CTRLSTATE.ROLL);
-        }
-
-        float delta = player.m_MoveSpeed.m_CurrentValue * Time.smoothDeltaTime;
-
+        float delta = m_MoveSpeed * Time.smoothDeltaTime;
         if (m_MoveData.MoveDist - delta < 0.0f)
         {
             delta = m_MoveData.MoveDist;
@@ -421,11 +377,11 @@ public class CharCtrl : Character
         if (m_MoveData.MoveDist < 0.01f)
         {
             UsingAni.SetBool("Walk", false);
-            ChangeCtrlState(CTRLSTATE.IDLE);
+            ChangeState(STATE.IDLE);
         }
     }
 
-    protected void Rotating(float KindOfRotate) // 캐릭터 회전함수
+    protected void Rotating(float KindOfRotate)
     {
         float delta = KindOfRotate * Time.smoothDeltaTime;
 
@@ -450,14 +406,15 @@ public class CharCtrl : Character
         {
             delta = 0f;
         }
+
         this.transform.Rotate(Vector3.up, delta);
     }
 
-    private void OnTriggerEnter(Collider obj) // 캐릭터 피격 검사 함수
+    private void OnTriggerEnter(Collider obj)
     {
-        if (obj.tag == "AtkBox" && ctrlstate == CTRLSTATE.IDLE)
+        if (obj.tag == "AtkBox" && state == STATE.IDLE)
         {
-            UsingAni.SetTrigger("Hit");
+            UsingAni.SetTrigger("Hit"); // 상체 애니메이션만 해야함
         }
     }
 }
@@ -466,4 +423,3 @@ public class CharCtrl : Character
 //Invoke("Restore", 0.1f); == 0.1초 뒤에 Restore라는 함수 호출 
 //GameObject.SendMessage == 해당 게임 오브젝트에 있는 특정 함수를 호출하기 위해 사용
 //DX11 랜더링은 버텍스 쉐이더와 픽셀 쉐이더가 한 쌍을 이루어야 작동함
-//투명도가 필요 없는 텍스쳐이면 알파포멧을 쓰지 않는게 좋음(용량문제)
