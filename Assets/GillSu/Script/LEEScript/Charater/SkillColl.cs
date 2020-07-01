88c5331c7203a93d4c86919c6615efe1313ca531
LeeGilsu <lgs731@naver.com>
2020-07-01 15:41:37 +0900

    ㅇㄴㄴ

---
+++ Assets/GillSu/Script/LEEScript/Charater/SkillColl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillColl : MonoBehaviour
+{
+    public void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("우왁!");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
